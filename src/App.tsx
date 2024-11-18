ad9856f3120c5f909f52cdd1b2343f5112d070c2
Haripradap <140403832+Haripradap@users.noreply.github.com>
2024-11-18 10:31:36 +0530

    project has been completed

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+
+
+import { Container, Typography } from '@mui/material'
+import './App.css'
+import AddHabitForm from './components/addHabitForm'
+import HabitList from './components/HabitList'
+import HabitStats from './store/habit-stats'
+
+function App() {
+  
+
+  return (
+    <>
+    <Container maxWidth='md' >
+      <Typography component='h1' variant='h2' align='center' >Habit Tracker</Typography>
+      <AddHabitForm/>
+      <HabitList/>
+      <HabitStats/>
+    </Container>
+    </>
+  )
+}
+
+export default App
